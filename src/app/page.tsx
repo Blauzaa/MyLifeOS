32649cee5359c883ea57193d6c65889d458147fb
Blauzaa <stevenstven21@gmail.com>
2025-12-25 21:24:11 +0700

    fix filename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { createClient } from '../utils/supabase/client'
+import { createClient } from '../utils/supabase/supabaseClient'
 import { Plus } from 'lucide-react'
 
 // 1. Kita definisikan bentuk datanya supaya TypeScript tidak marah
